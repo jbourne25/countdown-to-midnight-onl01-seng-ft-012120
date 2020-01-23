8ff3b5309db5e156ca3e69f67fbe3bc1de8cbe2a
Jeremy Bourne <jbourne25@yahoo.com>
2020-01-23 21:32:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 
  
 def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0 
+while seconds_to_midnight > 0 
   puts "#{seconds_to_midnight} SECOND(S)!"
   seconds_to_midnight -= 1 
-  end 
+end 
   "HAPPY NEW YEAR!"
 end  
 
