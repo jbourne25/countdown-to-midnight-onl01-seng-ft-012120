42c79d1e28b22f452d250615d1a69c786dbbfe6d
Jeremy Bourne <jbourne25@yahoo.com>
2020-01-23 21:34:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,11 +12,11 @@ end
 
 
 
-# def countdown(sleep)
-#   while number > 0 
-#   puts "#{number} SECOND(S)!"
-#   sleep(1)
-#   number -= 1 
-#   end
-#   "HAPPY NEW YEAR!"
-# end 
+def countdown(sleep)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  sleep(1)
+  number -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end 
